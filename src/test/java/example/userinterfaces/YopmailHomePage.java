d311093a7108aae38758f151aec713f13fea41eb
mschvap <matiasschvap@gmail.com>
2024-02-26 15:49:43 -0300

    Se cambia codigo en los test 3

--- src/test/java/example/userinterfaces/YopmailHomePage.java
+++ src/test/java/example/userinterfaces/YopmailHomePage.java
@@ -10,5 +10,4 @@ public class YopmailHomePage extends PageObject {
     public static final Target BUTTON_CORREO = Target.the("Boton de buscar correo").located(By.id("refreshbut"));
     public static final Target BUTTON_ACTIVAR_CUENTA = Target.the("Boton activar cuenta").locatedBy("//a[contains(text(),'Activar mi cuenta')]").inIFrame(By.xpath("//iframe[@id='ifmail']"));
     public static final Target BUTTON_VER_IMAGENES = Target.the("Boton ver imagenes").locatedBy("//span[contains(text(),'Ver las imágenes')]").inIFrame(By.xpath("//iframe[@id='ifmail']"));
-
 }
